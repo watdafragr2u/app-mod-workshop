57e0390af8515448c42d3952bd4b9eaa824f883e
Marco <marco.dellemonache@hotmail.it>
2024-11-08 10:47:40 +0000

    dockerize

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Configurazione del database
-$db_host = 'localhost';
+$db_host = '34.136.87.226';
 $db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+$db_user = 'user';
+$db_pass = 'mypassword';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
