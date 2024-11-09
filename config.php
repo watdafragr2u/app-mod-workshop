719d2d20d8e2ce21d70364cc1de8b8e2c99e5b77
Marco <marco.dellemonache@hotmail.it>
2024-11-09 00:49:01 +0000

    fix env

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Configurazione del database
-$db_host = '34.136.87.226';
-$db_name = 'image_catalog';
-$db_user = 'user';
-$db_pass = 'mypassword';
+$db_host = getenv('DB_HOST');
+$db_name = getenv('DB_NAME');
+$db_user = getenv('DB_USER');
+$db_pass = getenv('DB_PASS');
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
